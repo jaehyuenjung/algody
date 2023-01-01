eb314696d0d06cfe7cab76911f5654deb5881cc9
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2023-01-01 13:13:29 +0900

    enhancement(boj): 11659. 구간 합 구하기 4 - Java
    [변경 사항]
    # 숫자를 입력 받을 때 문자열로 받지 않고 byte 단위로 받아 바로 처리할 수 있도록 변경
    
    [성능]
    + 메모리: 58376 -> 32732 KB
    + 실행 시간: 1464 -> 1164 ms
    - 코드 길이: 950 -> 980 B

--- boj/prefix_sum/Q11659.java
+++ boj/prefix_sum/Q11659.java
@@ -2,29 +2,41 @@ package boj.prefix_sum;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.Reader;
 import utils.java.InputStreamReader;
 
 public class Q11659 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = nextInt(br);
+        int m = nextInt(br);
 
         int[] dp = new int[n + 2];
-        st = new StringTokenizer(br.readLine(), " ");
         for(int i = 1; i <= n; i++){
-            int v = Integer.parseInt(st.nextToken());
+            int v = nextInt(br);
             dp[i] = dp[i - 1] + v;
         }
 
         while(m-- > 0){
-            st = new StringTokenizer(br.readLine(), " ");
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
+            int i = nextInt(br);
+            int j = nextInt(br);
             System.out.println(dp[j] - dp[i - 1]);
         }
+
+        br.close();
+    }
+
+    static int nextInt(Reader reader) throws IOException {
+        int n = 0;
+        int c;
+        while((c = reader.read()) <= 32);
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while(isNumber(c = reader.read()));
+        return n;
+    }
+
+    static boolean isNumber(int c) {
+        return 47 < c && c < 58;
     }
 }
