c69d2a60756600c8e86417c7cef1b8881dac3d61
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2023-01-11 01:29:24 +0900

    feat(java): 자동 입력 파일 생성 기능 추가

--- utils/java/InputStreamReader.java
+++ utils/java/InputStreamReader.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
+import java.nio.file.Files;
 
 public class InputStreamReader extends Reader {
     private final java.io.InputStreamReader streamReader;
@@ -23,9 +24,21 @@ public class InputStreamReader extends Reader {
     }
 
     private File getAbsoluteResourceFile(String filePath) throws IOException {
-        File file = new File(getClass().getClassLoader().getResource(filePath).getPath());
-        if(!file.exists()){
-            file.createNewFile();
+        File file = null;
+        try {
+            file = new File(getClass().getClassLoader().getResource(filePath).getPath());
+        } catch (Exception e) {
+            String resourcePath = System.getProperty("user.dir") + "/resources/";
+
+            int sep = filePath.lastIndexOf("/");
+            String dictPath = filePath.substring(0, sep);
+
+            Files.createDirectories(new File(resourcePath + dictPath).toPath());
+            Files.createFile(new File(resourcePath + filePath).toPath());
+
+            System.out.println("*** 입력 파일이 없어서 생성합니다. ***");
+
+            file = new File(resourcePath + filePath);
         }
         return file;
     }
