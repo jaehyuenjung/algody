accfefef5d428bf077c0f37543fd9147b36e4edb
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2023-01-21 16:29:19 +0900

    solution(boj): 2435. 기상청 인턴 신현수 - Java

---
+++ boj/prefix_sum/Q2435.java
@@ -0,0 +1,46 @@
+package boj.prefix_sum;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import utils.java.InputStreamReader;
+
+public class Q2435 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = nextInt(br);
+        int k = nextInt(br) - 1;
+
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + nextInt(br);
+        }
+
+        int answer = -10000;
+        for (int i = 1; i <= n - k; i++) {
+            answer = Math.max(answer, dp[i + k] - dp[i - 1]);
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+
+    static int nextInt(Reader reader) throws IOException {
+        int n = 0;
+        int c;
+        boolean isNeg = false;
+        while ((c = reader.read()) <= 32 || isSign(c)) isNeg = c == 45;
+        do n = (n << 3) + (n << 1) + (c & 15);
+        while (isNumber(c = reader.read()));
+        return isNeg ? -n : n;
+    }
+
+    static boolean isNumber(int c) {
+        return 47 < c && c < 58;
+    }
+
+    static boolean isSign(int c) {
+        return c == 43 || c == 45;
+    }
+}
