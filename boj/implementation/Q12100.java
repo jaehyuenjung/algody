887ca6adc5ff166eb630a1631406faed574be9dd
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2023-02-25 18:33:27 +0900

    enhancement(boj): 12100. 2048 (Easy) - Java
    [변경 사항]
    # Union -> Move 프로세스를 동시에 처리할 수 있도록 변경
    
    [성능]
    + 메모리: 26032 -> 17108 KB
    + 실행 시간: 312 -> 156 ms
    + 코드 길이: 6199 -> 4224 B

--- boj/implementation/Q12100.java
+++ boj/implementation/Q12100.java
@@ -13,14 +13,17 @@ public class Q12100 {
 
         n = nextInt(br);
 
+        int maxVal = 0;
+
         int[][] map = new int[n][n];
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
                 map[r][c] = nextInt(br);
+                maxVal = Math.max(maxVal, map[r][c]);
             }
         }
 
-        System.out.println(dfs(0, 0, map));
+        System.out.println(Math.max(maxVal, dfs(0, 0, map)));
         br.close();
     }
 
@@ -29,181 +32,105 @@ public class Q12100 {
             return maxRlt;
         }
 
-        int[][] preMap = copyMap(map);
+        int[][] curMap = new int[n][n];
 
         int rlt = 0;
         for (int c = 0; c < n; c++) {
-            for (int r = 0; r < n - 1; r++) {
+            int p = 0;
+            curMap[p][c] = map[p][c];
+            for (int r = 1; r < n; r++) {
+                if (p < r) {
+                    curMap[r][c] = 0;
+                }
                 if (map[r][c] == 0) {
                     continue;
                 }
-                for (int k = r + 1; k < n; k++) {
-                    if (map[k][c] == 0) {
-                        continue;
-                    }
-                    if (map[r][c] == map[k][c]) {
-                        map[r][c] *= 2;
-                        map[k][c] = 0;
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-            }
 
-            for (int r = 0; r < n; r++) {
-                rlt = Math.max(rlt, map[r][c]);
-
-                if (r != 0 && map[r][c] != 0) {
-                    int d = 1;
-                    while (r - 1 * (d - 1) > 0 && map[r - 1 * d][c] == 0) {
-                        d++;
-                    }
-
-                    if (d - 1 > 0) {
-                        map[r - 1 * (d - 1)][c] = map[r][c];
-                        map[r][c] = 0;
-                    }
+                if (curMap[p][c] == 0) {
+                    curMap[p][c] = map[r][c];
+                } else if (curMap[p][c] == map[r][c]) {
+                    curMap[p][c] *= 2;
+                    rlt = Math.max(rlt, curMap[p++][c]);
+                } else {
+                    curMap[++p][c] = map[r][c];
                 }
             }
         }
-        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, map));
-
-        map = copyMap(preMap);
+        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, curMap));
 
         rlt = 0;
         for (int r = 0; r < n; r++) {
-            for (int c = n - 1; c > 0; c--) {
+            int p = n - 1;
+            curMap[r][p] = map[r][p];
+            for (int c = n - 2; c >= 0; c--) {
+                if (p > c) {
+                    curMap[r][c] = 0;
+                }
                 if (map[r][c] == 0) {
                     continue;
                 }
-                for (int k = c - 1; k >= 0; k--) {
-                    if (map[r][k] == 0) {
-                        continue;
-                    }
-                    if (map[r][c] == map[r][k]) {
-                        map[r][c] *= 2;
-                        map[r][k] = 0;
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-            }
 
-            for (int c = n - 1; c >= 0; c--) {
-                rlt = Math.max(rlt, map[r][c]);
-
-                if (c != n - 1 && map[r][c] != 0) {
-                    int d = 1;
-                    while (c + 1 * (d - 1) < n - 1 && map[r][c + 1 * d] == 0) {
-                        d++;
-                    }
-
-                    if (d - 1 > 0) {
-                        map[r][c + 1 * (d - 1)] = map[r][c];
-                        map[r][c] = 0;
-                    }
+                if (curMap[r][p] == 0) {
+                    curMap[r][p] = map[r][c];
+                } else if (curMap[r][p] == map[r][c]) {
+                    curMap[r][p] += map[r][c];
+                    rlt = Math.max(rlt, curMap[r][p--]);
+                } else {
+                    curMap[r][--p] = map[r][c];
                 }
             }
         }
-        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, map));
-
-        map = copyMap(preMap);
+        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, curMap));
 
         rlt = 0;
         for (int c = 0; c < n; c++) {
-            for (int r = n - 1; r > 0; r--) {
+            int p = n - 1;
+            curMap[p][c] = map[p][c];
+            for (int r = n - 2; r >= 0; r--) {
+                if (p > r) {
+                    curMap[r][c] = 0;
+                }
                 if (map[r][c] == 0) {
                     continue;
                 }
-                for (int k = r - 1; k >= 0; k--) {
-                    if (map[k][c] == 0) {
-                        continue;
-                    }
-                    if (map[r][c] == map[k][c]) {
-                        map[r][c] *= 2;
-                        map[k][c] = 0;
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-            }
 
-            for (int r = n - 1; r >= 0; r--) {
-                rlt = Math.max(rlt, map[r][c]);
-
-                if (r != n - 1 && map[r][c] != 0) {
-                    int d = 1;
-                    while (r + 1 * (d - 1) < n - 1 && map[r + 1 * d][c] == 0) {
-                        d++;
-                    }
-
-                    if (d - 1 > 0) {
-                        map[r + 1 * (d - 1)][c] = map[r][c];
-                        map[r][c] = 0;
-                    }
+                if (curMap[p][c] == 0) {
+                    curMap[p][c] = map[r][c];
+                } else if (curMap[p][c] == map[r][c]) {
+                    curMap[p][c] *= 2;
+                    rlt = Math.max(rlt, curMap[p--][c]);
+                } else {
+                    curMap[--p][c] = map[r][c];
                 }
             }
         }
-        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, map));
-
-        map = copyMap(preMap);
+        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, curMap));
 
         rlt = 0;
         for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n - 1; c++) {
+            int p = 0;
+            curMap[r][p] = map[r][p];
+            for (int c = 1; c < n; c++) {
+                if (p < c) {
+                    curMap[r][c] = 0;
+                }
                 if (map[r][c] == 0) {
                     continue;
                 }
-                for (int k = c + 1; k < n; k++) {
-                    if (map[r][k] == 0) {
-                        continue;
-                    }
-                    if (map[r][c] == map[r][k]) {
-                        map[r][c] *= 2;
-                        map[r][k] = 0;
-                        break;
-                    } else {
-                        break;
-                    }
-                }
-            }
 
-            for (int c = 0; c < n; c++) {
-                rlt = Math.max(rlt, map[r][c]);
-
-                if (c != 0 && map[r][c] != 0) {
-                    int d = 1;
-                    while (c - 1 * (d - 1) > 0 && map[r][c - 1 * d] == 0) {
-                        d++;
-                    }
-
-                    if (d - 1 > 0) {
-                        map[r][c - 1 * (d - 1)] = map[r][c];
-                        map[r][c] = 0;
-                    }
+                if (curMap[r][p] == 0) {
+                    curMap[r][p] = map[r][c];
+                } else if (curMap[r][p] == map[r][c]) {
+                    curMap[r][p] += map[r][c];
+                    rlt = Math.max(rlt, curMap[r][p++]);
+                } else {
+                    curMap[r][++p] = map[r][c];
                 }
             }
         }
-        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, map));
-
-        map = copyMap(preMap);
+        maxRlt = Math.max(maxRlt, dfs(cnt + 1, rlt, curMap));
 
         return maxRlt;
-    }
-
-    static int[][] copyMap(int[][] map) {
-        int[][] tempMap = new int[n][n];
-
-        for (int r = 0; r < n; r++) {
-            for (int c = 0; c < n; c++) {
-                tempMap[r][c] = map[r][c];
-            }
-        }
-
-        return tempMap;
     }
 
     static int nextInt(Reader reader) throws IOException {
